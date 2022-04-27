a5c08ee7e9d06fc468ec84e6ed54089b29843294
Your Name <90649822+LoneHilke@users.noreply.github.com>
2022-04-27 22:12:27 +0200

    part 1
    part 1
    log in side ser ikke ud som ved ham
    
    Co-Authored-By: Qvisty <18654175+qvisty@users.noreply.github.com>

---
+++ socialnetwork/landing/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views import View
+
+# Create your views here.
+class Index(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'landing/index.html')
+
+
+
+#fra: https://www.youtube.com/watch?v=BlXQCPsdcDc&list=PLPSM8rIid1a3TkwEmHyDALNuHhqiUiU5A&index=11
+#kommet til part 2
