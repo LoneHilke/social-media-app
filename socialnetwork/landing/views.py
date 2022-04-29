2145cf7601c5f4a126786dddd854e1023c9f7f7d
Your Name <90649822+LoneHilke@users.noreply.github.com>
2022-04-30 00:48:42 +0200

    part 5
    part 5
    med ca 10 min tilbage var der problemer med "NotImplementedError at /social/profile/edit/1/"
    
    Co-Authored-By: Qvisty <18654175+qvisty@users.noreply.github.com>

--- socialnetwork/landing/views.py
+++ socialnetwork/landing/views.py
@@ -9,4 +9,4 @@ class Index(View):
 
 
 #fra: https://www.youtube.com/watch?v=BlXQCPsdcDc&list=PLPSM8rIid1a3TkwEmHyDALNuHhqiUiU5A&index=11
-#kommet til part 4, 18:46
+#kommet til part 5, 24:28
--- socialnetwork/socialnetwork/settings.py
+++ socialnetwork/socialnetwork/settings.py
@@ -137,10 +137,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-LOGIN_REDIRECT_URL = 'post_list'
+LOGIN_REDIRECT_URL = 'post-list'
 ACCOUNT_EMAIL_REQUIRED = True
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
